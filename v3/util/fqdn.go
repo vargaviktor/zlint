d83cb50aaf6abbb54d71edb8d9cb241e022b3ba3
christopher-henderson <chris@chenderson.org>
2022-02-14 11:37:36 -0800

    no need for qualified package

--- v3/util/fqdn.go
+++ v3/util/fqdn.go
@@ -127,5 +127,5 @@ func IsLDHLabel(label string) bool {
 		!nonLDHCharacterRegex.MatchString(label) &&
 		!strings.HasPrefix(label, "-") &&
 		!strings.HasSuffix(label, "-") &&
-		!(util.HasReservedLabelPrefix(label) && !util.HasXNLabelPrefix(label))
+		!(HasReservedLabelPrefix(label) && !HasXNLabelPrefix(label))
 }
